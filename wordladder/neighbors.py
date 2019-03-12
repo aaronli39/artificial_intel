eade4310269b2b0daf78a2be7b756afe2e87104f
aaronli39 <aaronli39@gmail.com>
2019-03-12 14:35:30 -0400

    finished

--- wordladder/neighbors.py
+++ wordladder/neighbors.py
@@ -1,12 +1,51 @@
 import sys
 
+def do(inp, out, data):
+    alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    words = {}
+    # print(len(words))
+    for i in data:
+        ind = 0
+        neighbors = []
+        while ind < 4:
+            for letter in alpha:
+                word = i[:ind] + letter + i[ind + 1:]
+                if word in data:
+                    neighbors.append(word)
+            ind += 1
+        if i in neighbors:
+            neighbors.remove(i)
+
+        words[i] = neighbors
+    
+    writeStuff = ""
+    try:
+        with open(inp, "rU") as inFile:
+            search = inFile.read().split("\n")
+            for word in search:
+                writeStuff += word + "," + str(len(words.get(word))) + "\n"
+    except:
+        print("error reading input file")
+    
+    try:
+        with open(out, "w") as write:
+            write.write(writeStuff)
+    except:
+        print("error writing file")
+
 def main():
     try:
         with open("dictall.txt", "rU") as temp:
-            dat = temp.read().split("\n")
-            data = [i for i in dat if len(i) == 4]
-        # print(data, len(data))
+            data = set()
+            data = {word for word in temp.read().split("\n") if len(word) == 4}
+        # print(len(data))
+        # print(data)
     except:
         print("error while opening file.")
+
+    # do(sys.argv[1], sys.argv[2], data)
+    do("inp.txt", "out.txt", data)
+    # do(data)
 
 main()

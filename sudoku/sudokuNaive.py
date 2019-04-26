bee3660db5516e41b5bbfce23231243d40912d37
aaronli39 <aaronli39@gmail.com>
2019-04-26 17:37:20 -0400

    stuff

--- sudoku/sudokuNaive.py
+++ sudoku/sudokuNaive.py
@@ -5,6 +5,7 @@ inp = open(sys.argv[1], 'r')
 out = open(sys.argv[2], 'w')
 name_of_board = sys.argv[3]
 backtracks = 0
+board_state = []
 
 # size stuff
 SIZE = 9
@@ -14,7 +15,7 @@ board = []
 def getBoard():
     board = []
     lines = [x.strip() for x in inp.readlines()]
-    for i in range(1,10):
+    for i in range(1, 10):
         row = lines[lines.index(name_of_board) + i].strip().split(",")
         for i in range(9):
             if row[i] == '_':
@@ -24,15 +25,13 @@ def getBoard():
         board.append(row)
     return board
 
-theboard = getBoard()
-for i in range(9):
-    board.append(theboard[i])
+board = getBoard()
 
 # base case if the board is not solved yet
 def is_solved(row, col):
     isSolved = True
     for i in range(0, SIZE):
-        for j in range (0, SIZE):
+        for j in range(0, SIZE):
             # if cell isnt assigned
             if board[i][j] == 0:
                 row = i
@@ -47,7 +46,7 @@ def is_solved(row, col):
 def is_safe(n, r, c):
     # check if n is valid in row
     for i in range(0, SIZE):
-        #there is a cell with same value
+        # there is a cell with same value
         if board[r][i] == n:
             return False
 
@@ -62,10 +61,27 @@ def is_safe(n, r, c):
     row_start, col_start = (r // 3) * 3, (c // 3) * 3
     for i in range(row_start, row_start+3):
         for j in range(col_start, col_start+3):
-            if board[i][j]==n: return False
+            if board[i][j] == n:
+                return False
     return True
 
-# the actual solver
+def valid_list(r, c):
+    ret = []
+    for i in range(1, 10):
+        if is_safe(i, r, c):
+            ret.append(i)
+    return ret
+
+# smarter algo 1: fill in cells we are CERTAIN of
+for row in range(0, 9):
+    for col in range(0, 9):
+        if board[row][col] == 0:
+            temp = valid_list(row, col)
+            # print(temp)
+            if len(temp) == 1:
+                board[row][col] = temp[0]
+
+# recursive solver
 def solve():
     row, col = 0, 0
     global backtracks
@@ -75,23 +91,57 @@ def solve():
     row = a[0]
     col = a[1]
 
-    # number between 1 to 9
-    for i in range(1, 10):
+    lis = valid_list(row, col)
+    for i in lis:
         # check if i is a valid cell
-        if is_safe(i, row, col):
-            board[row][col] = i
-            
-            # backtracking
-            if solve(): 
-                return True
-            backtracks += 1
-            print(backtracks)
-            #reassign the cell
-            board[row][col] = 0
+        board[row][col] = i
+
+        # backtracking
+        if solve():
+            return True
+        # reassign the cell
+        backtracks += 1
+        board[row][col] = 0
+
+    return False
+
+def solve2():
+    row, col = 0, 0
+    # if all cells are assigned then the sudoku is already solved
+    a = is_solved(row, col)
+    if a[2]: return True
+    row = a[0]
+    col = a[1]
+    global backtracks
+    global board
+    global board_state
+
+    lis = valid_list(row, col)
+    for i in lis:
+        board_state.append(board)
+        # check if i is a valid cell
+        board[row][col] = i
+
+        # smarter algo 1: fill in cells we are CERTAIN of
+        for rows in range(0, 9):
+            for cols in range(0, 9):
+                if board[rows][cols] == 0:
+                    temp = valid_list(rows, cols)
+                    if len(temp) == 1:
+                        board[rows][cols] = temp[0]
+        # backtracking
+        if solve2():
+            return True
+        # reassign the cell
+        backtracks += 1
+        board[row][col] = 0
+        print(board_state)
+        board = board_state[len(board_state) - 1]
     return False
 
 def write():
-    solve()
+    solve2()
+    print(backtracks)
     # name_of_solved_board = name_of_board.replace('unsolved', 'solved') + '\n'
     # out.write(name_of_solved_board)
     for i in range(9):
--- wordladder/neighbors.py
+++ wordladder/neighbors.py
@@ -1,27 +1,160 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
 
 import sys
 
-def do(inp, out, data):
+def comp(a, b, end):
+        ac, bc = 0, 0
+        # print(a, b)
+        for i in range(4): 
+            if a[1][i] == end[i]: ac += 1
+        for i in range(4): 
+            if b[1][i] == end[i]: bc += 1
+        if (ac + a[0]) < (bc + b[0]): return 1
+        elif (ac + a[0]) == (bc + b[0]): return 0
+        return -1
+
+# priority queue
+class Pqueue:
+    # default comparison
+    # a and b are lists [steps, word]
+    def bad_cmp(self, a, b, end):
+        ac, bc = 0, 0
+        for i in range(self.len): 
+            if a[1][i] == end[i]: ac += 1
+        for i in range(self.len): 
+            if b[1][i] == end[i]: bc += 1
+        if (ac - a[0]) < (bc - b[0]): return -1
+        elif (ac - a[0]) == (bc - b[0]): return 0
+        return 1
+    
+    def ord_cmp(self, a, b, end):
+        ac, bc = 0, 0
+        for i in range(self.len): 
+            if a[1][i] == end[i]: ac += 1
+        for i in range(self.len): 
+            if b[1][i] == end[i]: bc += 1
+        if (ac - a[0]) < (bc - b[0]): return 1
+        elif (ac - a[0]) == (bc - b[0]): return 0
+        return -1
+
+    # init initializes the array, size, and comparator obj
+    def __init__(self, length, start, result, comparator=ord_cmp):
+        self.data = []
+        self.size = 0
+        self.len = length
+        self.end = result
+        self.cmp_to = comparator
+
+    # push takes in 1 val, appends it, and bubbles the
+    # val to appropriate position
+    def push(self, val):
+        self.data.append(val)
+        self.size += 1
+        
+        el = self.size - 1
+        par = int((self.size - 2) / 2)
+
+        while(self.cmp_to(self, self.data[el], self.data[par], self.end) == -1 and par >= 0):
+            self.data[el], self.data[par] = self.data[par], self.data[el]
+        
+            el = par
+            par = int((par - 1) / 2)
+
+    # pushL takes in a list, appends each value in list to 
+    # queue, and the bubbles them to their correct location 
+    def push_all(self, data):
+        [self.push(i) for i in data]
+        return
+
+    # pop root, rearrange rest of array
+    def pop(self):
+        if self.size == 0: return None
+
+        ret = self.data[0]
+        # set new root as last element to bubble down
+        self.data[0] = self.data[self.size - 1]
+        self.data, self.size = self.data[:-1], self.size - 1
+
+        # starting indices with left and right children
+        cur = 0
+        left, right = 2 * cur + 1, 2 * cur + 2
+
+        # if it's just a one level tree, pop root and swap left and current if necessary
+        if self.size == 2 and self.cmp_to(self, self.data[cur], self.data[left], self.end) == 1:
+            self.data[cur], self.data[left] = self.data[left], self.data[cur]
+
+        while(left < self.size and right < self.size and
+              (self.cmp_to(self, self.data[cur], self.data[left], self.end) == 1 or self.cmp_to(self, self.data[cur], self.data[right], self.end) == 1)):
+            # if left smaller than right
+            if self.cmp_to(self, self.data[left], self.data[right], self.end) == 1:
+                # right smaller so swap right and current
+                self.data[cur], self.data[right] = self.data[right], self.data[cur]
+                cur = right
+                left, right = cur * 2 + 1, cur * 2 + 2
+            else:
+                # swap left and current
+                self.data[cur], self.data[left] = self.data[left], self.data[cur]
+                cur = left
+                left, right = cur * 2 + 1, cur * 2 + 2
+        return ret
+
+    # look at next root
+    def peek(self):
+        if self.size == 0: return None
+        return self.data[0][1]
+
+    # returns list representation of the queue
+    def toList(self):
+        ret = [self.pop() for i in self.data]
+        return ret
+    
+    # returns the elements in the queue without popping
+    def internal_list(self):
+        return self.data
+
+# do the actual ladder
+def ladder(words, length, start, end):
+    queue = Pqueue(length, start, end)
+    neighbors, path, explored, steps = [start], [], set(), 0
+
+    while True:
+        steps += 1
+        for i in neighbors:
+            queue.push([steps, i, path])
+
+        # if no more elements there is no ladder
+        if len(queue.data) == 0: return [start, end]
+        val = queue.pop()
+        if val[1] == end: return val[2] + [val[1]]
+        else:
+            if val[1] not in explored:
+                explored.add(val[1])
+                path = val[2] + [val[1]]
+                neighbors = [word for word in words[val[1]] if word not in explored]
+            else:
+                path = []
+                neighbors = []
+
+# general function
+def do(inp, out, data, length):
     # vars to use
     alpha = "abcdefghijklmnopqrstuvwxyz"
     words = {}
-    writeStuff = ""
-    search = []
-    
-    # open file and make input as list
     try:
-        with open(inp, "rU") as inFile:
-            search = inFile.read().split("\n")
+        with open(inp, "rU") as temp:
+            temp = temp.read().split("\n")
+            goal = []
+            for i in temp:
+                goal.append(i.split(","))
     except:
         print("error reading input file")
         return
 
-    # create the dictionary of 4 letter words
+    # create the dictionary of words of desired length
     for i in data:
         ind = 0
         neighbors = set()
-        while ind < 4:
+        while ind < length:
             for letter in alpha:
                 word = i[:ind] + letter + i[ind + 1:]
                 if word in data and word != i:
@@ -30,36 +163,33 @@ def do(inp, out, data):
 
         words[i] = list(neighbors)
     
-    # create write data
-    if search[0] == "":
+    # getting wordladder data
+    if goal[0][0] == "":
         print("input file empty")
         return
+
+    ret = []
+    print(goal)
+    for i in goal:
+        ret.append(ladder(words, length, i[0], i[1]))
     
-    for word in search:
-        if not words.get(word):
-            writeStuff += word + ",0\n"
-        else:
-            writeStuff += word + "," + str(len(words.get(word))) + "\n"
-
-    # write to file        
-    try:
-        with open(out, "w") as write:
-            write.write(writeStuff)
-    except:
-        print("error writing file")
-        return
-
+    # create write data
+    with open(out, "w") as temp:
+        for i in ret:
+            temp.write(",".join(i) + "\n")
+    return
+    
+# sys argv stuff
 def main():
     try:
-        with open("dictall.txt", "rU") as temp:
-            data = set()
-            # create set of all 4 letter words to pass on
-            data = {word for word in temp.read().split("\n") if len(word) == 4}
+        with open(sys.argv[1], "rU") as temp:
+            length = len(temp.read().split("\n")[0].split(",")[0])
+            with open("dictall.txt", "rU") as temp:
+                data = set()
+                # create set of all 4 letter words to pass on
+                data = {word for word in temp.read().strip().split("\n") if len(word) == length}
     except:
-        print("error while opening file.")
-
-    do(sys.argv[1], sys.argv[2], data)
-    # do("inp.txt", "out.txt", data)
-    # do(data)
+        print("error getting input file")
+    do(sys.argv[1], sys.argv[2], data, length)
 
 main()

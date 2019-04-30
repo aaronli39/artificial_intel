1b9a6ca3dd6ab915ad66df79907ecdaed28163f4
aaronli39 <aaronli39@gmail.com>
2019-04-29 20:57:16 -0400

    stuff

--- sudoku/sudokuNaive.py
+++ sudoku/sudokuNaive.py
@@ -27,7 +27,6 @@ def getBoard():
     return board
 
 board = getBoard()
-print(board)
 
 # base case if the board is not solved yet
 def is_solved(row, col):
@@ -69,7 +68,7 @@ def is_safe(n, r, c):
 
 def valid_list(r, c):
     ret = []
-    for i in range(0, 10):
+    for i in range(1, 10):
         if is_safe(i, r, c):
             ret.append(i)
     return ret
@@ -120,7 +119,6 @@ def solve1(state):
                 for col in range(0, 9):
                     if board[row][col] == 0:
                         temp = valid_list(row, col)
-                        # print(temp)
                         if len(temp) == 1:
                             board[row][col] = temp[0]
     row, col = 0, 0
@@ -149,7 +147,16 @@ def solve1(state):
 
 # ------------------------------------------------------------------------
 # smarter algo 2: use the smallest guesses 
-def solve2():
+def solve2(state):
+    if state == 0:
+        for i in range(81):
+            for row in range(0, 9):
+                for col in range(0, 9):
+                    if board[row][col] == 0:
+                        temp = valid_list(row, col)
+                        if len(temp) == 1:
+                            # print(temp[0])
+                            board[row][col] = temp[0]
     # if all cells are assigned then the sudoku is already solved
     temp = get_smallest()
     a = is_solved(0, 0)
@@ -163,17 +170,85 @@ def solve2():
         board[row][col] = i
 
         # backtracking
-        if solve2():
+        if solve2(1):
             return True
         # reassign the cell
         backtracks += 1
-        print(backtracks)
         board[row][col] = 0
     return False
 # -------------------------------------------------------------------------
 
+def ret(lis):
+    for i in lis[:-1]: board[i[0]][i[1]] = i[2]
+
+def fill():
+    ret = []
+    for i in range(81):
+        for row in range(0, 9):
+            for col in range(0, 9):
+                if board[row][col] == 0:
+                    temp = valid_list(row, col)
+                    if len(temp) == 1:
+                        print("filling:", row + 1, col + 1)
+                        # print([row + 1, col + 1, temp[0]])
+                        # for i in range(9): print(','.join([str(x) for x in board[i]]))
+                        # print("---------------------------")
+                        ret.append([row, col, temp[0]])
+                        board[row][col] = temp[0]
+                    elif len(temp) == 0: 
+                        print("asdfasdfasd")
+                        ret.append(False)
+                        return ret
+    # print(ret)
+    ret.append(True)
+    return ret
+
+# ------------------------------------------------------------------------
+# smarter algo 3: use the smallest guesses and fill forced cells
+def solve3(state, back):
+    if state == 0:
+        for i in range(81):
+            for row in range(0, 9):
+                for col in range(0, 9):
+                    if board[row][col] == 0:
+                        temp = valid_list(row, col)
+                        if len(temp) == 1:
+                            board[row][col] = temp[0]
+    # if all cells are assigned then the sudoku is already solved
+    temp = get_smallest()
+    print("smallest", temp)
+    a = is_solved(0, 0)
+    if a[2]: return True
+    row = temp[0]
+    col = temp[1]
+    global backtracks
+
+    for i in temp[2][:-1]:
+        # check if i is a valid cell
+        board[row][col] = i
+        baccc = fill()
+        print("info", row, col, i, baccc)
+        if baccc[len(baccc) - 1] == False: 
+            backtracks += 1
+            board[row][col] = 0
+            ret(back)
+            continue
+        # backtracking
+        if solve3(1, baccc): 
+            return True
+        # reassign the cell
+        backtracks += 1
+        # print("back", back)
+        board[row][col] = 0
+        ret(back)
+    return False
+# -------------------------------------------------------------------------
+
 def write():
-    solve2()
+    # print(solve3(0, []))
+    solve2(0)
+    # solve1(0)
+    print(backtracks)
     # name_of_solved_board = name_of_board.replace('unsolved', 'solved') + '\n'
     # out.write(name_of_solved_board)
     for i in range(9):
